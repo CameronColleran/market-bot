758190d4b87f521d4a8eb5f0c861d18102874ca6
Cameron <cameroncolleran3@gmail.com>
2022-08-29 00:40:39 -0700

    refactor schedule time constants

--- main.py
+++ main.py
@@ -6,14 +6,18 @@ from eod import tweet_end_of_day_message
 from insight import give_insight
 from random_stock import tweet_random_stock
 
+HOURLY = ":00"
+DAILY = "17:30"
+SECOND_INTERVAL = 10
+
 # schedule jobs
-schedule.every().hour.at(":00").do(tweet_random_stock)
-schedule.every().monday.at("17:30").do(tweet_end_of_day_message)
-schedule.every().tuesday.at("17:30").do(tweet_end_of_day_message)
-schedule.every().wednesday.at("17:30").do(tweet_end_of_day_message)
-schedule.every().thursday.at("17:30").do(tweet_end_of_day_message)
-schedule.every().friday.at("17:30").do(tweet_end_of_day_message)
-schedule.every(10).seconds.do(give_insight)
+schedule.every().hour.at(HOURLY).do(tweet_random_stock)
+schedule.every().monday.at(DAILY).do(tweet_end_of_day_message)
+schedule.every().tuesday.at(DAILY).do(tweet_end_of_day_message)
+schedule.every().wednesday.at(DAILY).do(tweet_end_of_day_message)
+schedule.every().thursday.at(DAILY).do(tweet_end_of_day_message)
+schedule.every().friday.at(DAILY).do(tweet_end_of_day_message)
+schedule.every(SECOND_INTERVAL).seconds.do(give_insight)
 
 while True:
     schedule.run_pending()
